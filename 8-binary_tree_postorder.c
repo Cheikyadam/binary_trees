a96535a0530ed2d3c5c7205263a90c7abc500459
Cheikyadam <urbaingande@gmail.com>
2024-01-30 10:48:38 -0800

    8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - postorder traversal
+ * @tree: the tree
+ * @func: a function
+ *
+ * Return: Nothing
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (func == NULL)
+		return;
+	if (tree == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
