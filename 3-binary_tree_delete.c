f9f35a58eaccb7ea99b1235291f0b89de6dc432f
Cheikyadam <urbaingande@gmail.com>
2024-01-30 09:24:08 -0800

    3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,13 +9,27 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
+	binary_tree_t *tmp = NULL;
+
 	if (tree == NULL)
 		return;
 	if (tree != NULL && tree->left == NULL && tree->right == NULL)
 	{
-		free(tree);
-		tree = NULL;
+		tmp = tree;
+		if (tree->parent != NULL)
+		{
+			if (tree->parent->left == tree)
+				tree->parent->left = NULL;
+			if (tree->parent->right == tree)
+				tree->parent->right = NULL;
+		}
+		free(tmp);
+		tmp = NULL;
 	}
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
+	else
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree);
+	}
 }
