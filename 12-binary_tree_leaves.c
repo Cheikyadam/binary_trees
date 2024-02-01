3b7774e180961942636783f55327c5f61a09fe9c
Cheikyadam <urbaingande@gmail.com>
2024-02-01 06:08:05 -0800

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,5 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	if (tree->parent != NULL && tree->left == NULL && tree->right == NULL)
 		return (1);
+	if (tree->parent == NULL && tree->left == NULL && tree->right == NULL)
+		return (0);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
