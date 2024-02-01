5bd2ef78fc93667f07eb9cbaab52b699737484ef
Cheikyadam <urbaingande@gmail.com>
2024-02-01 15:13:40 -0800

    100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Common ancestor
+ * @first: first node
+ * @second: scond node
+ *
+ * Return: First common parent
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first->left == second || first->right == second)
+		return (first->left->parent);
+	if (second->left == first || second->right == first)
+		return (second->left->parent);
+	if (second->parent == first->parent)
+		return (second->parent);
+	return (binary_trees_ancestor(second->parent, first->parent));
+}
